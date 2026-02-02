3ece87b63b87556a7fc4a4dd397d24734cb58903
Charlie <2321322144@qq.com>
2026-02-02 15:39:58 +0800

    add qrcode feishu key getting play address

--- src/routers/monitor/urls.py
+++ src/routers/monitor/urls.py
@@ -4,13 +4,15 @@ import urllib.parse
 from PIL import Image
 import os
 import base64
+import requests
+from requests_toolbelt import MultipartEncoder
 from .ezviz_stream_manage import DeviceStream
 from src.utils import get_qrcode_buffer
 
 monitor = APIRouter()
 
 @monitor.get("/playAddress")
-async def get_play_address(request: Request, device_id: str = Query(...)):
+async def get_play_address(request: Request, device_id: str = Query(...), feishu_authorization: str = Query(None)):
     try:
         device_stream = DeviceStream(device_id)
         if device_stream.hls_address is None:
@@ -28,12 +30,33 @@ async def get_play_address(request: Request, device_id: str = Query(...)):
 
     play_qrcode_base64 = base64.b64encode(play_qrcode_buffer.getvalue()).decode("utf-8")
 
-    return JSONResponse(
-        content={
-            "playAddress": play_address,
-            "qrcodeURL": qrcode_url,
-            "qrcodeBase64": play_qrcode_base64,
-            "startTime": device_stream.start_time,
-            "endTime": device_stream.end_time
-        },
-        status_code=200)
+    response_data = {
+        "playAddress": play_address,
+        "qrcodeURL": qrcode_url,
+        "qrcodeBase64": play_qrcode_base64,
+        "startTime": device_stream.start_time,
+        "endTime": device_stream.end_time
+    }
+
+    if feishu_authorization:
+        try:
+            url = "https://open.feishu.cn/open-apis/im/v1/images"
+            form = {
+                'image_type': 'message',
+                'image': ('play_qrcode.png', play_qrcode_buffer, 'image/png')
+            }
+            multi_form = MultipartEncoder(form)
+            headers = {
+                'Authorization': feishu_authorization,
+                'Content-Type': multi_form.content_type
+            }
+            response = requests.post(url, headers=headers, data=multi_form)
+            if response.status_code == 200:
+                response_data["qrcodeFeishuKey"] = response.json()["data"]["image_key"]
+            else:
+                return JSONResponse(content={"error": f"Fail to get qrcode feishu key: response = {response.content}"},
+                                    status_code=404)
+        except Exception as e:
+            return JSONResponse(content={"error": f"Fail to get qrcode feishu key: {e}"}, status_code=404)
+
+    return JSONResponse(content=response_data, status_code=200)
